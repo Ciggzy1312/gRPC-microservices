9cf7dc35b656aba10c523288d92d77b078ce46a8
Ciggzy1312 <deepayanmukherjee1312@gmail.com>
2023-01-01 17:10:13 +0530

    Setup post service server
    Signed-off-by: Ciggzy1312 <deepayanmukherjee1312@gmail.com>

---
+++ post_service/index.js
@@ -0,0 +1,21 @@
+const PROTO_PATH = __dirname + '/protos/post.proto';
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const { createServer } = require('./grpc');
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
+
+const post_proto = grpc.loadPackageDefinition(packageDefinition);
+
+const server = new grpc.Server();
+createServer(server);
+
+server.addService(post_proto.PostService.service, {
+    // Add services here
+});
